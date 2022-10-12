2485410d23f47b227c93c42b82c03a5bd9cbe235
a1conwf <anton.kashuba@gmail.com>
2022-10-12 12:28:47 +0300

    Make nav fixed on mobile, add transition from right to left, fix desktop version

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,12 @@ import "./Header.scss";
 import logo from "../../assets/brand/logo.svg";
 import hamburgerIcon from "../../assets/svg/icon-hamburger.svg";
 import closeIcon from "../../assets/svg/icon-close.svg";
+import logoDark from "../../assets/brand/logo-dark.svg";
+import hamburgerIconDark from "../../assets/svg/icon-hamburger-dark.svg";
 
 const Header = () => {
 	const [isExpanded, setIsExpanded] = useState(false);
+	const [isDark, setIsDark] = useState(false);
 
 	useEffect(() => {
 		window.addEventListener("resize", () => {
@@ -14,52 +17,47 @@ const Header = () => {
 				setIsExpanded(false);
 			}
 		});
+		window.addEventListener("scroll", () => {
+			if (window.scrollY >= 610) {
+				setIsDark(true);
+			} else {
+				setIsDark(false);
+			}
+		});
 	}, []);
 
 	return (
 		<header className="header">
 			<div className="container">
-				<nav className="header__nav">
-					<div className="logo">
-						<img src={logo} alt="logo-img" />
-					</div>
+				<div className="header__nav">
+					<a className="logo" href="#!">
+						{isDark && !isExpanded ? <img src={logoDark} alt="logo-img" /> : <img src={logo} alt="logo-img" />}
+					</a>
 
-					<ul className="header__nav-list hover">
-						<li>
-							<a href="#!">About</a>
-						</li>
-						<li>
-							<a href="#!">Careers</a>
-						</li>
-						<li>
-							<a href="#!">Events</a>
-						</li>
-						<li>
-							<a href="#!">Products</a>
-						</li>
-						<li>
-							<a href="#!">Support</a>
-						</li>
-					</ul>
+					<nav className={isExpanded ? "nav active" : "nav"}>
+						<ul className={isExpanded ? "nav__list" : "nav__list hover"}>
+							<li>
+								<a href="#!">About</a>
+							</li>
+							<li>
+								<a href="#!">Careers</a>
+							</li>
+							<li>
+								<a href="#!">Events</a>
+							</li>
+							<li>
+								<a href="#!">Products</a>
+							</li>
+							<li>
+								<a href="#!">Support</a>
+							</li>
+						</ul>
+					</nav>
 
 					<div className="hamburger" onClick={() => setIsExpanded(!isExpanded)}>
-						{isExpanded ? <img src={closeIcon} alt="close-icon" /> : <img src={hamburgerIcon} alt="hamburger-icon" />}
+						{isDark && !isExpanded ? <img src={hamburgerIconDark} alt="hamburger-icon" /> : !isDark && !isExpanded ? <img src={hamburgerIcon} alt="icon-hamburger" /> : <img src={closeIcon} alt="icon-close" />}
 					</div>
-
-					{isExpanded && (
-						<nav className="header__nav-mobile">
-							<ul>
-								<li>
-									<a href="#!">About</a>
-									<a href="#!">Careers</a>
-									<a href="#!">Events</a>
-									<a href="#!">Products</a>
-									<a href="#!">Support</a>
-								</li>
-							</ul>
-						</nav>
-					)}
-				</nav>
+				</div>
 
 				<div className="header__content">
 					<h1 className="header__content-title">Immersive experiences that deliver</h1>
